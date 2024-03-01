23d6a69374d74aef0310b0ed432b19f499071287
Kervie19 <114479831+Kervie19@users.noreply.github.com>
2024-03-01 15:02:14 +0800

    signup&login

--- DocAS/Program.cs
+++ DocAS/Program.cs
@@ -16,7 +16,7 @@ namespace DocAS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Signup());
+            Application.Run(new Login());
         }
     }
 }
